6aca3a91bfe95991778592363bd91077b49e82c1
Nhat Khanh <nhatkhanh9a3@gmail.com>
2023-06-11 23:11:45 +0700

    refactor projects tab

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -46,7 +46,7 @@ export const Projects = () => {
       <Container>
         <Row>
           <Col size={12}>
-            <TrackVisibility partialVisibility>
+            {/* <TrackVisibility partialVisibility>
               {({ isVisible }) => (
                 <div
                   className={
@@ -112,7 +112,58 @@ export const Projects = () => {
                   </Tab.Container>
                 </div>
               )}
-            </TrackVisibility>
+            </TrackVisibility> */}
+            <div>
+              <h2>Projects</h2>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.
+              </p>
+              <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                <Nav
+                  variant="pills"
+                  className="nav-pills mb-5 justify-content-center align-items-center"
+                  id="pills-tab"
+                >
+                  <Nav.Item>
+                    <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+                <Tab.Content id="slideInUp">
+                  <Tab.Pane eventKey="first">
+                    <Row>
+                      {projects.map((project, index) => {
+                        return <ProjectCard key={index} {...project} />;
+                      })}
+                    </Row>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="section">
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Cumque quam, quod neque provident velit, rem explicabo
+                      excepturi id illo molestiae blanditiis, eligendi dicta
+                      officiis asperiores delectus quasi inventore debitis quo.
+                    </p>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="third">
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Cumque quam, quod neque provident velit, rem explicabo
+                      excepturi id illo molestiae blanditiis, eligendi dicta
+                      officiis asperiores delectus quasi inventore debitis quo.
+                    </p>
+                  </Tab.Pane>
+                </Tab.Content>
+              </Tab.Container>
+            </div>
           </Col>
         </Row>
       </Container>
